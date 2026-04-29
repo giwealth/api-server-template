ec650347cbe14c93d1152be8b3d7afd5f7f451d5
zengming <zengming@juwang.cn>
2026-04-29 17:57:57 +0800

    修复验证header问题

--- internal/presentation/httpapi/middlewares/validate.go
+++ internal/presentation/httpapi/middlewares/validate.go
@@ -1,18 +1,20 @@
 package middlewares
 
 import (
+	"mime"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 // ValidateHeaders 用于API接口POST, PUT检查请求头
 func ValidateHeaders(ctx *gin.Context) {
-	headers := ctx.Request.Header
 	method := ctx.Request.Method
 	if method == "POST" || method == "PUT" {
-		t := headers["Content-Type"]
-		if len(t) == 0 || t[0] != "application/json" {
+		contentType := strings.TrimSpace(ctx.GetHeader("Content-Type"))
+		mediaType, _, err := mime.ParseMediaType(contentType)
+		if err != nil || mediaType != "application/json" {
 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "content-type header is not application/json"})
 			return
 		}
